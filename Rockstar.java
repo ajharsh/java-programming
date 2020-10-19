b37f25904a63821b7f480875a2f4244691838b76
ajharsh <72903712+ajharsh@users.noreply.github.com>
2020-10-19 18:51:31 +0530

    Add files via upload
    examples for object and class program

---
+++ Rockstar.java
@@ -0,0 +1,6 @@
+class Rockstar{
+String name,college,place,course;
+void garduate(){
+System.out.println(name + " is graduated from " + college + place + " in " + course + " branch"  );
+ }
+}                  
---
+++ RockstarDetails.java
@@ -0,0 +1,34 @@
+class RockstarDetails{
+public static void main(String[] args){
+Rockstar Harshitha = new Rockstar();
+Harshitha.name = " Harshitha ";
+Harshitha.college=" AIT ";
+Harshitha.place=" Chikkamagaluru ";
+Harshitha.course=" ECE ";
+Harshitha.garduate();
+Rockstar Chinmayi = new Rockstar();
+Chinmayi.name = " Chinmayi ";
+Chinmayi.college=" AIT ";
+Chinmayi.place=" Chikkamagaluru ";
+Chinmayi.course=" ISE ";
+Chinmayi.garduate();
+Rockstar Girija = new Rockstar();
+Girija.name = " Girija ";
+Girija.college=" BGMIT ";
+Girija.place=" Mudhol";
+Girija.course=" ECE ";
+Girija.garduate();
+Rockstar Sameer = new Rockstar();
+Sameer.name = " Sameer ";
+Sameer.college=" BGMIT ";
+Sameer.place=" Mudhol ";
+Sameer.course=" MEE ";
+Sameer.garduate();
+Rockstar Karthik = new Rockstar();
+Karthik.name = " Karthik ";
+Karthik.college=" APS ";
+Karthik.place=" Bangalore ";
+Karthik.course=" ECE ";
+Karthik.garduate();
+}
+}
